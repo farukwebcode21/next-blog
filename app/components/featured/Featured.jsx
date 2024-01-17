3fd2f4766e89659b9926ad1f66ca68aaab8599af
farukwebcode21 <farukahmadmk@yahoo.com>
2024-01-17 11:55:09 +0600

    now working featar page

--- app/components/featured/Featured.jsx
+++ app/components/featured/Featured.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import style from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={style.container}>Featured</div>;
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>
+        <b>Hey, lama dev here !</b> Discourer my stories and creative idea.
+      </h1>
+      <div className={style.post}>
+        <div className={style.imgContainer}>
+          <Image src="/p1.jpeg" alt="image" fill className={style.image} />
+        </div>
+        <div className={style.textContainer}>
+          <h1 className={style.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing porro tempora
+            alias in laudantium?
+          </h1>
+          <p className={style.postDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            praesentium error qui ipsum, doloribus eius totam dolore
+            consequuntur aperiam perferendis nisi commodi eos reiciendis
+            doloremque laborum unde accusantium expedita nesciunt aspernatur ad
+            labore magni atque suscipit vero! Quod amet deleniti perspiciatis
+            cupiditate tenetur inventore ipsam cumque,
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
